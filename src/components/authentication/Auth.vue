c4ba7aa302b3648c2bb262a65a78bc8911802159
JosephLi <williamlifaith@gmail.com>
2017-10-29 22:09:45 -0500

    added two sample files into two empty folders so that folders can appear in github

---
+++ src/components/authentication/Auth.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>Hello 4-quadrants</h1>
+    This is Auth component!
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '',
+    data () {
+      return {
+        msg: ''
+      }
+    }
+  }
+</script>
+
+<style lang="sass">
+h1
+
+  color: #ffff00
+
+
+</style>
