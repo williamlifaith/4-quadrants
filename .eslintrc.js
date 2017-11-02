5b010b7aa574b696329f13060ceddbb121b27847
Somebody <free.server.github@gmail.com>
2017-11-02 10:16:02 +0800

    [framework] Provide better eslint for vue project #3

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,20 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
     sourceType: 'module'
   },
   env: {
     browser: true,
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
+  // extends: 'standard',
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended' // or 'plugin:vue/base'
+  ],
   // required to lint *.vue files
   plugins: [
     'html'
